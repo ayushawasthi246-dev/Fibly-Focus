8eaa52afaf2f9ee650beec8e8da374fdc485f43d
Ayush <ayushawasthi2246@gmail.com>
2025-11-07 15:31:04 +0530

    changed the frontend link in server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const port = process.env.port || 4000;
 connectdb();
 
-const allowedorigin = ['http://localhost:5173']
+const allowedorigin = ['https://fiblyfocus.vercel.app']
 
 app.use(cookieParser());
 app.use(cors({ origin : allowedorigin , credentials: true }));
